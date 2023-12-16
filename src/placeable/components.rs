70b03f814b9f8a036c816d6e1deeaa66c41bf759
Devin Bidwell <dbidwell94@biddydev.com>
2023-12-15 17:34:27 -0700

    generic placeable item

--- src/placeable/components.rs
+++ src/placeable/components.rs
@@ -1,20 +1,56 @@
 use bevy::prelude::*;
 
-pub trait PlaceableItem:
-    Sync + Send + core::fmt::Debug + core::hash::Hash + PartialEq + Eq + Clone
-{
+pub trait PlaceableItem: Sync + Send {
+    fn max_resources(&self) -> usize;
+    fn current_resources(&self) -> usize;
 }
 
-#[derive(Component)]
+macro_rules! placeables {
+    (
+        $(
+            struct $name: ident {
+                $(
+                    $field: ident: $ty: ty
+                ),* $(,)?
+            }
+        ),*
+    ) => {
+        $(
+            #[derive(Component, Debug, Copy, Clone)]
+            pub struct $name {
+                $(
+                    pub $field: $ty,
+                )*
+                pub max_resources: usize,
+                pub current_resources: usize,
+            }
+
+            impl PlaceableItem for $name {
+                fn max_resources(&self) -> usize {
+                    self.max_resources
+                }
+
+                fn current_resources(&self) -> usize {
+                    self.current_resources
+                }
+            }
+        )*
+    };
+}
+
+#[derive(Component, Clone)]
 pub struct Placeable<T: PlaceableItem + ?Sized>(pub Box<T>);
 
 #[derive(Component)]
 pub struct Tileable;
 
-#[derive(Component)]
-pub struct Wall {
-    pub health: usize,
-}
+placeables!(
+    struct Wall {},
+    struct Turret {}
+);
 
-#[derive(Bundle)]
-pub struct WallBundle {}
+#[derive(Bundle, Clone)]
+pub struct PlaceableBundle<T: PlaceableItem + ?Sized + 'static> {
+    pub placeable: Placeable<T>,
+    pub sprite_bundle: SpriteBundle,
+}
--- src/placeable/mod.rs
+++ src/placeable/mod.rs
@@ -3,8 +3,12 @@ mod systems;
 
 use bevy::prelude::*;
 
+use self::components::{PlaceableItem, PlaceableBundle};
+
 pub struct PlaceablePlugin;
 
 impl Plugin for PlaceablePlugin {
     fn build(&self, app: &mut App) {}
 }
+
+pub struct CurrentPlaceableItem(pub Option<PlaceableBundle<dyn PlaceableItem>>);

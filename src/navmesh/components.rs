b239ece39672d8939cfa64191c3de2512af634e3
Devin Bidwell <dbidwell94@biddydev.com>
2023-12-16 15:53:44 -0700

    removed unused type in Navmesh

--- src/navmesh/components.rs
+++ src/navmesh/components.rs
@@ -1,4 +1,3 @@
-use crate::utils::*;
 use crate::SIZE;
 use bevy::{prelude::*, utils::HashSet};
 
@@ -34,31 +33,6 @@ pub struct PathfindRequest {
     pub start: Vec2,
     pub end: Vec2,
     pub entity: Entity,
-}
-
-pub type PathfindingQuery<'world, 'state> = Query<'world, 'state, Entity>;
-
-impl PathfindRequest {
-    pub fn from_query(
-        requesting: Entity,
-        to_entity: Entity,
-        query: &PathfindingQuery,
-    ) -> Option<Self> {
-        let requesting_transform = query.get_component::<Transform>(requesting).ok()?;
-        let to_entity_transform = query.get_component::<Transform>(to_entity).ok()?;
-
-        Some(Self {
-            start: requesting_transform
-                .translation
-                .truncate()
-                .world_pos_to_tile(),
-            end: to_entity_transform
-                .translation
-                .truncate()
-                .world_pos_to_tile(),
-            entity: requesting,
-        })
-    }
 }
 
 #[derive(Debug, Event)]
